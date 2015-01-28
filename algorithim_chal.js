8a476e8130f4be1cbbf9eba72b71702236668f3d
Kyle Baker <finesttype@gmail.com>
2015-01-28 17:00:50 -0600

    alg_chal_major_edit
    edited algorithms challenge
    Took off the training wheels, think I'm good to go

---
+++ algorithim_chal.js
@@ -0,0 +1,21 @@
+var findSmallestDifference = function(arr) {
+    var differences = [];
+    arr.sort(function(a,b){return a - b});
+    for(a=0;a<arr.length;a++){
+        for(b=a+1;b<arr.length;b++){
+            differences[differences.length] = arr[b] - arr[a];
+        } 
+    }
+    var answer = (differences[0] + differences[1])
+    for (d=0;d<differences.length;d++){
+            if (differences[d] < differences[d+1]) {
+                    var bestOfTwo = differences[d]
+                    if (bestOfTwo < answer) {
+                        answer = bestOfTwo;
+                    }
+            }
+    }
+    return answer;
+};
+var result = findSmallestDifference([1829,10,100,1000,10000]);
+console.log(result);
